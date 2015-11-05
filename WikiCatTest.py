Fixed some normalization bugs, moved to root directory structure, and started analysis

WikiCatTest.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 from os import getcwd, mkdir
 from itertools import chain
 from scipy.stats import entropy
+import sys
 import numpy as np
 from os.path import (
     join,
@@ -28,28 +29,39 @@ if __name__ == "__main__":
 
     arg_parser.add_argument("url", action='store', help='Valid (i.e. URLEncoded) article URL')
 
+    """
     arg_parser.add_argument("-m", "--model-file", action='store', help='Path to\
                             where the trained model should be loaded from. Defaults\
                             to /tmp/WikiCat/model.pkl. Non-absolute paths are\
                             considered relative to the current directory.', dest='model_file',
                             default='/tmp/WikiCat/model.pkl')
+    """
 
-    arg_parser.add_argument("-r", "--repr-file", action='store', help='Path to\
-                            where the representer (i.e. normalization params,\
-                            vocab) object is stored.', dest='repr_file',
-                            default='/tmp/WikiCat/repr.pkl')
+    arg_parser.add_argument("-r", "--root-dir", action='store', help='Path to\
+                            where the representer, model, and cache are stored.\
+                            Defaults to /tmp/WikiCat',
+                            dest='root_dir', default='/tmp/WikiCat/')
 
+    """
     arg_parser.add_argument("-c", "--cache", action='store', help="Directory containing the\
                             downloaded GloVe vector files.",
                             dest='cache', default='/tmp/WikiCat/cache')
-
+    """
     args = arg_parser.parse_args()
+
+    root_dir = get_fullpath(args.root_dir)
+    model_fn = join(root_dir, 'model.pkl')
+    repr_fn = join(root_dir, 'repr.pkl')
+    if not (exists(model_fn) and exists(repr_fn)):
+        print("Please run WikiCatBuild.py first and make sure that you use the "
+              "same root directory")
+        sys.exit(1)
+
     article = ArticlePage(args.url, verbosity=args.verbosity)
-    model = load_obj(get_fullpath(args.model_file), verbosity=args.verbosity)
-    embedder = load_obj(get_fullpath(args.repr_file), verbosity=args.verbosity)
+    model = load_obj(model_fn, verbosity=args.verbosity)
+    embedder = load_obj(repr_fn, verbosity=args.verbosity)
     #embedder = Representer(get_fullpath(args.cache), verbosity=args.verbosity)
     article_rep = embedder.transform([article.Content()])
-    #print(article_rep)
     model_predictions = model.predict_proba(article_rep).flatten()
     for ind, class_prob in enumerate(model_predictions):
         print("%s: %0.3f" % (model.ind_label_map[ind], class_prob))

WikiCatUtils.py
@@ -363,7 +363,7 @@ class Cache:
                 category_article_map[article.Category()] = [article.Content()]
 
         category_label_map = {category:ind for ind, category in
-                          enumerate(category_article_map.keys())}
+                          enumerate(sorted(category_article_map.keys()))}
         dset = {
             'train':[],
             'val':[],
@@ -502,7 +502,10 @@ class Representer:
             print("Determining normalization constants")
         if self.normalize:
             self.normalize = False
+            tmp_v = self.verbosity
+            self.verbosity = 0
             to_normalize = self.transform(dataset)
+            self.verbosity = tmp_v
             self.normalize = True
             self.maxes = np.max(to_normalize, axis=0)
             self.mins = np.min(to_normalize, axis=0)
@@ -531,7 +534,7 @@ class Representer:
             print("%d words were OOV out of %d total" % (oov, total_words))
         vecs = np.vstack(vecs)
         if self.normalize:
-            vecs = (vecs - self.maxes) / (self.maxes - self.mins)
+            vecs = (vecs - self.mins) / (self.maxes - self.mins)
             vecs[np.where(np.isnan(vecs))] = 0.5
         return vecs
 